1d2ccc790c43ca748c2910b32d6ee0e8c9b29f64
Tijmen Zwaan <tijmen.zwaan@gmail.com>
2012-06-20 11:33:34 +0200

    Cube class toegevoegd.
    cube class met verschillende vlakken als arrays.

---
+++ Rubiks Cube/src/rubiks/Cube/Cube.java
@@ -0,0 +1,21 @@
+package rubiks.Cube;
+
+public class Cube {
+	int[] top = new int[9];
+	int[] left = new int[9];
+	int[] front = new int[9];
+	int[] right = new int[9];
+	int[] bottom = new int[9];
+	int[] back = new int[9];
+	
+	public Cube() {
+		for(int i = 0; i < 9; i++) {
+			top[i] = 0;
+			left[i] = 1;
+			front[i] = 2;
+			right[i] = 3;
+			bottom[i] = 4;
+			back[i] = 5;
+		}
+	}
+}
